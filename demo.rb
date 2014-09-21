d3a7ac3cb151676634735efe653aad9afb0ab6ac
CJ Avilla <cjavilla@gmail.com>
2014-09-20 19:58:35 -0700

    adding a ruby demo file for testing the api endpoints

---
+++ demo.rb
@@ -0,0 +1,40 @@
+require 'rest_client'
+
+filing = {
+  sum_shares: 200,
+  sum_shares_after: 200,
+  day_traded_volume: 200,
+  day_traded_price: 200,
+  is_officer: 1,
+  is_director: 0,
+  is_ten_percent_owner: 0,
+  is_other: 0,
+  per_code_p: 100,
+  per_code_s: 0,
+  per_code_v: 0,
+  per_code_a: 0,
+  per_code_d: 0,
+  per_code_f: 0,
+  per_code_i: 0,
+  per_code_m: 0,
+  per_code_c: 0,
+  per_code_e: 0,
+  per_code_h: 0,
+  per_code_o: 0,
+  per_code_x: 0,
+  per_code_g: 0,
+  per_code_l: 0,
+  per_code_w: 0,
+  per_code_z: 0,
+  per_code_j: 0,
+  per_code_k: 0,
+  per_code_u: 0
+}
+
+puts RestClieint.post(
+  'http://localhost:8765/predict',
+  filing.to_json,
+  content_type: :json,
+  accept: :json
+).body
+
